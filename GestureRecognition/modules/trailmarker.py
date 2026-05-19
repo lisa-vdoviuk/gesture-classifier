58eecaeb1c5b2062608d8e1a4a63fe4f9732eb6c
Lisa <yelyzaveta.vdoviuk@gmail.com>
2026-05-19 15:28:33 +0200

    Upgraded TrailMarker module

--- GestureRecognition/modules/trailmarker.py
+++ GestureRecognition/modules/trailmarker.py
@@ -1,39 +1,79 @@
 from SignalHub import Module, get_nested_key, GALY
 from collections import deque
 import numpy as np
+import mediapipe as mp
+from pathlib import Path
+import cv2
+import time
+
+BaseOptions = mp.tasks.BaseOptions
+GestureReqognizer = mp.tasks.vision.GestureRecognizer
+GestureReqognizerOptions = mp.tasks.vision.GestureRecognizerOptions
+VisionRunningMode = mp.tasks.vision.RunningMode
+
 
 class TrailMarker(Module):
     def __init__(self, outputSignal="trailmarker"):
         super().__init__(
-            inputSignals=["config","detector", "webcam"],
+            inputSignals=["config","training_controller","detector", "webcam"],
             outputSchema={"type": "object", "properties": {outputSignal: {}}},
             name="trailmarker",
         )
         self.outputSignal = outputSignal
 
     def start(self, data):
+        gesture_model_path = Path("gesture_recognizer.task")
         self.trail = deque(maxlen=60) # Creating a list with max of 60 variables.
         self.lost_frames = 0
         self.max_lost_frames = 5
         self.index_finger = 8
-        return {}
+        options = GestureReqognizerOptions(
+            base_options=BaseOptions(model_asset_path="gesture_recognizer.task"),
+            running_mode=VisionRunningMode.VIDEO,
+        )
+        self._recognizer = GestureReqognizer.create_from_options(options)
+
+        return {
+
+        }
 
     def step(self, data):
         result = data["detector"]
         frame = data["webcam"]
+        frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
-        if result and getattr(result, "hand_landmarks", None):
-            # Grabs the very first hand it sees (Hand 0)
-            hand = result.hand_landmarks[0]
-            x = hand[self.index_finger].x
-            y = hand[self.index_finger].y
+        if frame_rgb.dtype != np.uint8:
+            if frame_rgb.max() <= 1.0:
+                frame_rgb = (np.clip(frame_rgb, 0.0, 1.0) * 255).astype(np.uint8)
+            else:
+                frame_rgb = np.clip(frame_rgb, 0, 255).astype(np.uint8)
 
-            self.trail.append((x,y))
-            self.lost_frames = 0
-        else:
-            self.lost_frames+=1
-            if self.lost_frames >= self.max_lost_frames:
-                self.trail.clear()
+        frame_rgb = np.ascontiguousarray(frame_rgb)
+        mp_image = mp.Image(
+            image_format=mp.ImageFormat.SRGB,
+            data=frame_rgb,
+        )
+
+        timestamp_ms = int(time.time() * 1000)
+        result = self._recognizer.recognize_for_video(mp_image, timestamp_ms)
+        if result.gestures:
+            category = result.gestures[0][0]
+            gesture = category.category_name
+            score = category.score
+
+            self.current_gesture = f"{gesture} {score:.2f}"
+            if gesture == "Pointing_Up":
+                # Grabs the very first hand it sees (Hand 0)
+                hand = result.hand_landmarks[0]
+                x = hand[self.index_finger].x
+                y = hand[self.index_finger].y
+
+                self.trail.append((x,y))
+                self.lost_frames = 0
+            elif gesture == "Close_Fist":
+                self.lost_frames+=1
+                if self.lost_frames >= self.max_lost_frames:
+                    self.trail.clear()
         
         galy = GALY()
         galy.layer("trail", alwaysVisible=True)
