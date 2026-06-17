190467ebf176e30e054e1c5d9493a1755448e66b
Lisa <yelyzaveta.vdoviuk@gmail.com>
2026-06-17 16:25:42 +0200

    Deleted extra files

--- run_labeling.py
+++
@@ -1,6 +0,0 @@
-from labeling import data_labeling, dataset_building
-label = input("Enter label(figure, gesture): ")
-count = int(input("How many samples: "))
-
-data_labeling(count, label)
-dataset_building("data/dataset.pkl")
